8818c1324d218623e27ae4f373935d92b3a86e26
MiaoF <miaofang@gmail.com>
2014-09-16 08:44:15 +0200

    This is project is created for debugging My Magicdraw Plugin in Eclipse.

--- Debug Plugins/src/Debug.java
+++ Debug Plugins/src/Debug.java
@@ -1,21 +1,16 @@
-import mvmf.topology.WMF_MD_TopologyPlugin;
-import myplugin.MyPlugin;
-
+import magicdraw.feature.editor.FeatureEditor;
 import com.nomagic.magicdraw.core.Application;
 import com.nomagic.runtime.ApplicationExitedException;
 
 public class Debug {
 	public static void main(String[] args) {
 		try {
-			MyPlugin plugin = new MyPlugin();
-			WMF_MD_TopologyPlugin topoPlugin = new WMF_MD_TopologyPlugin();
+			FeatureEditor plugin = new FeatureEditor();
 			Application app = Application.getInstance();
 			app.start(true, false, false, args, null);
 			plugin.init();
-			topoPlugin.init();
 		} catch (ApplicationExitedException e) {
 			e.printStackTrace();
 		}
 	}
-
 }
